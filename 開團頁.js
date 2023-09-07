0853a256857e90928d71127a4f015d56ee4b9bd2
Lun-01 <143683655+Lun-01@users.noreply.github.com>
2023-09-07 15:45:33 +0800

    導覽列結束

--- 開團頁.js
+++ 開團頁.js
@@ -16,7 +16,7 @@ searchInput.addEventListener('blur', function () {
     searchInput.style = "none"
 });
 
-// 下拉清單
+// 探索下拉清單
 const exploreLink = document.getElementById('explore-link');
 const exploreDropdown = document.getElementById('explore-dropdown');
 
@@ -27,3 +27,37 @@ exploreLink.addEventListener('mouseenter', function () {
 exploreDropdown.addEventListener('mouseleave', function () {
     exploreDropdown.style.display = 'none';
 });
+
+
+// 通知下拉清單
+const notify = document.getElementById('notify');
+const notifyDropdown = document.getElementById('notify-dropdown');
+
+
+notify.addEventListener('click', function (e) {
+    e.stopPropagation(); // 阻止點擊事件冒泡
+    notifyDropdown.style.display = 'block';
+  });
+  
+  document.addEventListener('click', function () {
+    notifyDropdown.style.display = 'none';
+  });
+  
+  notifyDropdown.addEventListener('click', function (e) {
+    e.stopPropagation(); // 阻止點擊事件冒泡
+  });
+
+  //通知數字
+  function updateBadge(number) {
+    const badge = document.getElementById('badgeNumber');
+    const notificationBadge = document.getElementById('notificationBadge');
+    
+    badge.innerText = number;
+  
+    const width = 16 + (number > 9 ? 6 : 0) + (number > 99 ? 6 : 0); // 調整寬度
+    const height = 16; // 固定高度
+  
+    notificationBadge.style.width = `${width}px`;
+    notificationBadge.style.height = `${height}px`;
+  }
+  updateBadge(999);
