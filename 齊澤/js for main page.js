0a029e4257cf211e9aae4ed3ae222063dca355e9
JH3436 <chemical3436@gmail.com>
2023-09-07 15:48:01 +0800

    main page_box-update

--- 齊澤/js for main page.js
+++ 齊澤/js for main page.js
@@ -19,6 +19,7 @@ searchInput.addEventListener('blur', function () {
 // 下拉清單
 const exploreLink = document.getElementById('explore-link');
 const exploreDropdown = document.getElementById('explore-dropdown');
+const groupLink = document.getElementById("group-link");
 
 exploreLink.addEventListener('mouseenter', function () {
     exploreDropdown.style.display = 'block';
@@ -26,6 +27,11 @@ exploreLink.addEventListener('mouseenter', function () {
 
 exploreDropdown.addEventListener('mouseleave', function () {
     exploreDropdown.style.display = 'none';
+});
+
+// 確保移動到"發起揪團"懸浮視窗關閉
+groupLink.addEventListener('mouseenter', function () {
+  exploreDropdown.style.display = 'none';
 });
 
 //moving letter特效
