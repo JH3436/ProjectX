4daf531846ab4aab8dec7e26e94e0b1f4f191554
samuel772014 <samuel772014@gmail.com>
2023-09-08 13:49:49 +0800

    dialogd開啟關閉_samuel0908

--- activityPage_samuel/js/sign.js
+++ activityPage_samuel/js/sign.js
@@ -3,15 +3,28 @@
     let btn=document.querySelector("#signBtn");
     let infoModal=document.querySelector("#signDialog");
     let close=document.querySelector("#dialogClose");
+    let sign=document.querySelector("#dialogSign");
     btn.addEventListener("click", function(){
       infoModal.showModal();
     })
     close.addEventListener("click", function(){
       infoModal.close();
     })
+    sign.addEventListener("click", function(){
+      console.log($('calendar'));
+      confirm('確定資料正確');
+      infoModal.close();
+    })
 
+    
     $(() => {
-      $('#dialogSign').click(function(){
-        confirm('確定資料正確');
-      });
+      // $('#signBtn').click(function(){
+      //   $('#signDialog').show();
+
+      // });
+      
+      // $('#dialogSign').click(function(){
+      //   // confirm('確定資料正確');
+      //   $(this).close();
+      // });
     })
