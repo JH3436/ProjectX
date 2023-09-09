90cbf88779ffd724f841724f58dbfc6c032e1ddd
JH3436 <chemical3436@gmail.com>
2023-09-10 01:36:51 +0800

    活動方塊改版
    現在有按愛心的功能，方塊呈現樣式：活動分類、活動標題、活動天數、主揪是誰。

--- 齊澤/js for main page.js
+++ 齊澤/js for main page.js
@@ -7,11 +7,11 @@ const groupLink = document.getElementById("group-link");
 
 
 exploreLink.addEventListener('mouseenter', function () {
-    exploreDropdown.style.display = 'block';
+  exploreDropdown.style.display = 'block';
 });
 
 exploreDropdown.addEventListener('mouseleave', function () {
-    exploreDropdown.style.display = 'none';
+  exploreDropdown.style.display = 'none';
 });
 
 // 確保移動到"發起揪團"懸浮視窗關閉
@@ -24,17 +24,17 @@ groupLink.addEventListener('mouseenter', function () {
 var textWrapper = document.querySelector('.ml14 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({ loop: true })
   .add({
     targets: '.ml14 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
+    scaleX: [0, 1],
+    opacity: [0.5, 1],
     easing: "easeInOutExpo",
     duration: 900
   }).add({
     targets: '.ml14 .letter',
-    opacity: [0,1],
-    translateX: [40,0],
+    opacity: [0, 1],
+    translateX: [40, 0],
     translateZ: 0,
     scaleX: [0.3, 1],
     easing: "easeOutExpo",
@@ -48,3 +48,41 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+//活動方塊-愛心設計
+const heartIcons = document.querySelectorAll('.heart-icon');
+
+heartIcons.forEach(function (heartIcon) {
+  heartIcon.addEventListener('click', function () {
+    if (heartIcon.classList.contains('fa-regular')) {
+      heartIcon.classList.remove('fa-regular');
+      heartIcon.classList.add('fa-solid');
+      heartIcon.style.color = "#B44163";
+
+      const cardInfo = heartIcon.closest('.card').querySelector('.card__info');
+
+      const likedText = document.createElement('span');
+      likedText.textContent = '已收藏';
+      likedText.classList.add('liked-text');
+
+      cardInfo.appendChild(likedText);
+    } else {
+      heartIcon.classList.remove('fa-solid');
+      heartIcon.classList.add('fa-regular');
+      heartIcon.style.color = "#1E3050";
+      // 移除已經存在的 "Liked" 文字
+      const likedText = heartIcon.closest('.card').querySelector('.liked-text');
+      if (likedText) {
+        likedText.remove();
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
