6f6d0452600bc2570ab1ba1bfc192f3424edc102
JH3436 <chemical3436@gmail.com>
2023-09-10 14:34:50 +0800

    小更動

--- 齊澤/js for main page.js
+++ 齊澤/js for main page.js
@@ -80,6 +80,44 @@ heartIcons.forEach(function (heartIcon) {
 });
 
 
+// 介紹網站測試
+$(function(){
+  $('.mycarousel-item').eq(0).addClass('active');
+  var total = $('.mycarousel-item').length;
+  var current = 0;
+  $('#moveRight').on('click', function(){
+    var next=current;
+    current= current+1;
+    setSlide(next, current);
+  });
+  $('#moveLeft').on('click', function(){
+    var prev=current;
+    current = current- 1;
+    setSlide(prev, current);
+  });
+  function setSlide(prev, next){
+    var slide= current;
+    if(next>total-1){
+     slide=0;
+      current=0;
+    }
+    if(next<0){
+      slide=total - 1;
+      current=total - 1;
+    }
+           $('.mycarousel-item').eq(prev).removeClass('active');
+           $('.mycarousel-item').eq(slide).addClass('active');
+      setTimeout(function(){
+
+      },800);
+    
+
+    
+    console.log('current '+current);
+    console.log('prev '+prev);
+  }
+});
+
 
 
 
