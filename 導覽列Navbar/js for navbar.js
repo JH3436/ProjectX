e68f8a0161c2da6c8ca378643aedadbc4370e49e
JH3436 <chemical3436@gmail.com>
2023-09-20 11:50:15 +0800

    登入後導覽列_通知完成

--- 導覽列Navbar/js for navbar.js
+++ 導覽列Navbar/js for navbar.js
@@ -1,5 +1,5 @@
 // 導覽列
-$(document).ready(function() {
+$(document).ready(function () {
   // 選取探索按鈕
   var exploreLink = $("#explore-link");
   // 選取下拉內容
@@ -7,35 +7,59 @@ $(document).ready(function() {
   //選取揪團按鈕
   var groupBtn = $("#group-link");
 
-  exploreLink.hover(
-    function() {
-      // 滑鼠進入時顯示下拉內容
-      exploreDropdown.css("display", "block");
-    
+  exploreLink.hover(function () {
+    // 滑鼠進入時顯示下拉內容
+    exploreDropdown.css("display", "block");
   });
 
   exploreDropdown.hover(
-    function() {
+    function () {
       // 滑鼠進入下拉內容時保持顯示
       exploreDropdown.css("display", "block");
     },
-    function() {
+    function () {
       // 滑鼠離開下拉內容時隱藏
       exploreDropdown.css("display", "none");
     }
   );
-    //滑鼠移動到揪團也會讓下拉消失
-    groupBtn.hover(
-      function() {
-        exploreDropdown.css("display", "none")
-      }
-    )
+  //滑鼠移動到揪團也會讓下拉消失
+  groupBtn.hover(function () {
+    exploreDropdown.css("display", "none");
+  });
 
-    //點擊鈴鐺後，數字通知消失
-    // 使用事件委託，當點擊<i>元素時執行以下操作
-  $('.notification').on('click', 'i', function() {
+
+
+//點擊鈴鐺後，數字通知消失
+// 使用事件委託，當點擊<i>元素時執行以下操作
+  $(".notification").on("click", "i", function () {
     // 移除包含.notification--num的元素
-    $(this).parent().find('.notification--num').remove();
+    $(this).parent().find(".notification--num").remove();
   });
 });
 
+//鈴鐺+下拉
+$(".notification").on("click", function () {
+  const ele = $(".notification-popup");
+  if (ele.hasClass("active")) {
+    console.log("remove active");
+    ele.removeClass("active");
+  } else {
+    console.log("add active");
+    ele.addClass("active");
+  }
+});
+
+document.addEventListener(
+  "click",
+  function (event) {
+      // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+      if (
+          event.target.closest(".notification") == null &&
+          event.target.closest(".notification-popup") == null
+      ) {
+          console.log("remove active click outside");
+          $(".notification-popup").removeClass("active");
+      }
+  },
+  false
+);
