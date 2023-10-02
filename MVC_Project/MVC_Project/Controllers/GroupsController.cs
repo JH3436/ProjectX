06b3345207d9e73e6b92087a3636bc0321b31164
Lun-01 <143683655+Lun-01@users.noreply.github.com>
2023-10-02 21:51:26 +0800

    Group頁面完美重現

---
+++ MVC_Project/MVC_Project/Controllers/GroupsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly ProjectXContext _context;
+
+        public GroupsController(ProjectXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Groups
+        public async Task<IActionResult> Index()
+        {
+            return _context.Group != null ?
+                          View(await _context.Group.ToListAsync()) :
+                          Problem("Entity set 'ProjectXContext.Group'  is null.");
+        }
+
+        // GET: Groups/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Group == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Group
+                .FirstOrDefaultAsync(m => m.GroupID == id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return View(@group);
+        }
+
+        // GET: Groups/Create
+        public IActionResult Create()
+        {
+            ViewData["Organizer"] = new SelectList(_context.Member, "UserID", "UserID");
+            ViewData["OriginalActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID");
+            return View();
+        }
+
+        // POST: Groups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GroupID,GroupName,GroupCategory,GroupContent,MinAttendee,MaxAttendee,StartDate,EndDate,Organizer,OriginalActivityID")] Group @group)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@group);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Organizer"] = new SelectList(_context.Member, "UserID", "UserID", @group.Organizer);
+            ViewData["OriginalActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID", @group.OriginalActivityID);
+            return View(@group);
+        }
+
+        // GET: Groups/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Group == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Group.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+            ViewData["Organizer"] = new SelectList(_context.Member, "UserID", "UserID", @group.Organizer);
+            ViewData["OriginalActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID", @group.OriginalActivityID);
+            return View(@group);
+        }
+
+        // POST: Groups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GroupID,GroupName,GroupCategory,GroupContent,MinAttendee,MaxAttendee,StartDate,EndDate,Organizer,OriginalActivityID")] Group @group)
+        {
+            if (id != @group.GroupID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@group);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GroupExists(@group.GroupID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Organizer"] = new SelectList(_context.Member, "UserID", "UserID", @group.Organizer);
+            ViewData["OriginalActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID", @group.OriginalActivityID);
+            return View(@group);
+        }
+
+        // GET: Groups/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Group == null)
+            {
+                return NotFound();
+            }
+
+            var @group = await _context.Group
+                .FirstOrDefaultAsync(m => m.GroupID == id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return View(@group);
+        }
+
+        // POST: Groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Group == null)
+            {
+                return Problem("Entity set 'ProjectXContext.Group'  is null.");
+            }
+            var @group = await _context.Group.FindAsync(id);
+            if (@group != null)
+            {
+                _context.Group.Remove(@group);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GroupExists(int id)
+        {
+          return (_context.Group?.Any(e => e.GroupID == id)).GetValueOrDefault();
+        }
+    }
+}
