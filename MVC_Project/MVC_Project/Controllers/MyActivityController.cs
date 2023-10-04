c0f9a4d9bb74a2beb6716a12f9c046b2c67223b1
y30224 <y30224@gmail.com>
2023-10-04 16:14:41 +0800

    --

---
+++ MVC_Project/MVC_Project/Controllers/MyActivityController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class MyActivityController : Controller
+    {
+        private readonly ProjectXContext _context;
+
+        public MyActivityController(ProjectXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MyActivity
+        public async Task<IActionResult> Index()
+        {
+              return _context.MyActivity != null ? 
+                          View(await _context.MyActivity.ToListAsync()) :
+                          Problem("Entity set 'ProjectXContext.MyActivity'  is null.");
+        }
+
+        // GET: MyActivity/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.MyActivity == null)
+            {
+                return NotFound();
+            }
+
+            var myActivity = await _context.MyActivity
+                .FirstOrDefaultAsync(m => m.ActivityID == id);
+            if (myActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(myActivity);
+        }
+
+        // GET: MyActivity/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MyActivity/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActivityID,ActivityName,Category,SuggestedAmount,ActivityContent,MinAttendee,VoteDate,ExpectedDepartureMonth")] MyActivity myActivity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(myActivity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(myActivity);
+        }
+
+        // GET: MyActivity/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.MyActivity == null)
+            {
+                return NotFound();
+            }
+
+            var myActivity = await _context.MyActivity.FindAsync(id);
+            if (myActivity == null)
+            {
+                return NotFound();
+            }
+            return View(myActivity);
+        }
+
+        // POST: MyActivity/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActivityID,ActivityName,Category,SuggestedAmount,ActivityContent,MinAttendee,VoteDate,ExpectedDepartureMonth")] MyActivity myActivity)
+        {
+            if (id != myActivity.ActivityID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(myActivity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MyActivityExists(myActivity.ActivityID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(myActivity);
+        }
+
+        // GET: MyActivity/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.MyActivity == null)
+            {
+                return NotFound();
+            }
+
+            var myActivity = await _context.MyActivity
+                .FirstOrDefaultAsync(m => m.ActivityID == id);
+            if (myActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(myActivity);
+        }
+
+        // POST: MyActivity/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.MyActivity == null)
+            {
+                return Problem("Entity set 'ProjectXContext.MyActivity'  is null.");
+            }
+            var myActivity = await _context.MyActivity.FindAsync(id);
+            if (myActivity != null)
+            {
+                _context.MyActivity.Remove(myActivity);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MyActivityExists(int id)
+        {
+          return (_context.MyActivity?.Any(e => e.ActivityID == id)).GetValueOrDefault();
+        }
+    }
+}
