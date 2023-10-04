f309b492e212600d2026a4b50636c82569915727
DORA0301 <143470054+DORA0301@users.noreply.github.com>
2023-10-04 14:55:46 +0800

    member/likeRecords完成

---
+++ MVC_Project/MVC_Project/Controllers/MemberController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+
+    public class MemberController : Controller
+    {
+         private readonly ProjectXContext _context;
+
+            public MemberController(ProjectXContext context)
+            {
+                _context = context;
+            }
+
+
+            public IActionResult Registration()
+        {
+
+            return View();
+        }
+        public IActionResult LikeRecord()
+        {
+            var userId = 1; // 從當前登錄的使用者取得UserId
+
+            // 取得這個UserId所有的LikeRecords以及相對應的MyActivity資料
+            var likedActivities = _context.LikeRecord
+    .Where(lr => lr.UserID == userId)
+    .Include(lr => lr.Activity)
+    .Select(lr => new LikedActivityViewModel
+    {
+        ActivityName = lr.Activity.ActivityName,
+        VoteDate = lr.Activity.VoteDate
+    })
+    .ToList();
+
+            ViewBag.LikedActivities = likedActivities;
+
+            return View();
+        }
+        public IActionResult Member()
+        {
+            return View();
+        }
+        public IActionResult PersonGroup()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Project/MVC_Project/Models/LikedActivityViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC_Project.Models
+{
+    public class LikedActivityViewModel
+    {
+        public string ?ActivityName { get; set; }
+        public DateTime? VoteDate { get; set; }
+    }
+}
