ec65a5cc761eae818d31422290eafd78c1f6d310
samuel772014 <samuel772014@gmail.com>
2023-10-07 22:25:13 +0800

    留言版功能

---
+++ MVC_Project/MVC_Project/Controllers/ActivityController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Models;
+
+
+namespace MVC_Project.Controllers
+{
+    public class ActivityController : Controller
+    {
+        private  ProjectXContext _context;
+
+        public ActivityController(ProjectXContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ActivityController
+        public IActionResult Index(int? id)
+        {
+            if (id == null || _context.MyActivity == null)
+            {
+                return NotFound();
+            }
+            var data =
+            from m in _context.MyActivity
+            join o in _context.OfficialPhoto
+            on m.ActivityID equals o.ActivityID
+            where m.ActivityID == id
+            select new responseActivity
+            {
+                ActivityName = m.ActivityName,
+                Category = m.Category,
+                SuggestedAmount = m.SuggestedAmount,
+                ActivityContent = m.ActivityContent,
+                MinAttendee = m.MinAttendee,
+                VoteDate = m.VoteDate,
+                ExpectedDepartureMonth = m.ExpectedDepartureMonth,
+                PhotoPath = o.PhotoPath
+            };
+
+
+            var temp = data.ToList();
+            
+            return View(temp);
+        }
+
+        
+        // POST: ActivityController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Chat chat)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ActivityController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ActivityController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_Project/MVC_Project/Controllers/groupPageController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using MVC_Project.Models;
+
+
+namespace MVC_Project.Controllers
+{
+    public class groupPageController : Controller
+    {
+        private ProjectXContext _context;
+        // GET: groupPageController
+        public groupPageController(ProjectXContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult groupPage(int? id)
+        {
+            if (id == null || _context.MyActivity == null)
+            {
+                return NotFound();
+            }
+            var data = from g in _context.Group
+                       join c in _context.Chat
+                       on g.GroupID equals c.ActivityID
+                       where g.GroupID == id
+                       select new Group
+                       {
+                           GroupID = g.GroupID,
+                           GroupName = g.GroupName,
+                           GroupCategory = g.GroupCategory,
+                           GroupContent = g.GroupContent,
+                           MinAttendee = g.MinAttendee,
+                           MaxAttendee = g.MaxAttendee,
+                           StartDate = g.StartDate,
+                           EndDate = g.EndDate,
+                           Organizer = g.Organizer,
+                           Chat = _context.Chat.Where(chat => chat.ActivityID == g.GroupID).ToList(),
+                           OrganizerNavigation = _context.Member.FirstOrDefault(m => m.UserID == g.Organizer), // Set OrganizerNavigation based on Member Id
+                           OriginalActivity = _context.MyActivity.FirstOrDefault(a => a.ActivityID == g.OriginalActivityID), // Set OriginalActivity based on ActivityID
+                           PersonalPhoto = _context.PersonalPhoto.Where(pp => pp.GroupID == g.GroupID).ToList(), // Set PersonalPhoto based on GroupID
+                           Registration = _context.Registration.Where(r => r.GroupID == g.GroupID).ToList() // Set Registration based on GroupID
+
+                       };
+
+            //var data = from g in _context.Group
+            //                      join p in _context.PersonalPhoto
+            //                      on g.GroupID equals p.GroupID
+            //                      join c in _context.Chat
+            //                      on g.GroupID equals c.ActivityID
+            //                      where g.GroupID == id
+            //                      select new Group
+            //                      {
+            //                          GroupName = g.GroupName,
+            //                          GroupCategory = g.GroupCategory,
+            //                          GroupContent = g.GroupContent,
+            //                          MinAttendee = g.MinAttendee,
+            //                          MaxAttendee = g.MaxAttendee,
+            //                          StartDate = g.StartDate,
+            //                          EndDate = g.EndDate,
+            //                          Organizer = g.Organizer,
+            //                          Chat = _context.Chat.Where(chat => chat.ActivityID == g.GroupID).ToList(),
+            //                          OrganizerNavigation = _context.Member.FirstOrDefault(m => m.UserID == g.Organizer), // Set OrganizerNavigation based on Member Id
+            //                          OriginalActivity = _context.MyActivity.FirstOrDefault(a => a.ActivityID == g.OriginalActivityID), // Set OriginalActivity based on ActivityID
+            //                          PersonalPhoto = _context.PersonalPhoto.Where(pp => pp.GroupID == g.GroupID).ToList(), // Set PersonalPhoto based on GroupID
+            //                          Registration = _context.Registration.Where(r => r.GroupID == g.GroupID).ToList() // Set Registration based on GroupID
+
+            //                      };
+
+
+
+            var temp = data.ToList();
+
+            return View(temp);
+        }
+
+
+
+        // POST: groupPageController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+    }
+}
