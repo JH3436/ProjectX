345f318d43d580c999d938cfe3a81a238888ad0e
JH3436 <chemical3436@gmail.com>
2023-10-15 21:45:33 +0800

    投票選擇功能_暫存點
    投票選項更新還沒做好

--- MVC_Project/MVC_Project/Controllers/VoteRecordsController.cs
+++ MVC_Project/MVC_Project/Controllers/VoteRecordsController.cs
@@ -18,12 +18,60 @@ namespace MVC_Project.Controllers
             _context = context;
         }
 
-        //齊澤寫的
-        public IActionResult SelectDate(int Activityid)
+        //James寫的
+        // GET: /VoteRecords/SelectDate/{ActivityId}
+        public IActionResult SelectDate(int? id, string VoteResult)
         {
+            //接收來自點擊通知的ActivityID
+            var activityId = id;
+
+            //假設使用者目前是1
+            var userId = 1;
+
+            //查找是否已經投票
+            var userHasVoted = _context.VoteRecord
+           .Any(vr => vr.UserID == userId && vr.ActivityID == activityId);
+
+            ViewBag.UserHasVoted = userHasVoted;
+
+            // 如果用戶已經投票，獲取用戶上一次的投票選項
+            if (userHasVoted)
+            {
+                var lastVote = _context.VoteRecord
+                    .Where(vr => vr.UserID == userId && vr.ActivityID == activityId)
+                    .OrderByDescending(vr => vr.RecordID)
+                    .Select(vr => vr.VoteResult)
+                    .FirstOrDefault();
+
+                ViewBag.LastVote = lastVote?.ToString("yyyy-MM-dd");
+            }
+
+
+            //根據 ActivityId 查詢相應的活動投票時間
+            var voteTimes = _context.VoteTime
+                .Where(vt => vt.ActivityID == activityId)
+                .ToList();
+
+            ViewBag.Dates = voteTimes.Select(vt => vt.StartDate).ToList();
+
+
+            // 寫入資料，檢查是否有選擇了投票選項
+            if (VoteResult != null)
+            {
+                var voteRecord = new VoteRecord
+                {
+                    UserID = userId, // 這裡應該設置為當前使用者的 ID
+                    ActivityID = activityId,
+                    VoteResult = DateTime.Parse(VoteResult) // 將選擇的日期轉換為 DateTime
+                };
+
+                _context.VoteRecord.Add(voteRecord);
+                _context.SaveChanges();
+            }
 
             return View();
         }
+        
 
 
 
@@ -79,29 +127,30 @@ namespace MVC_Project.Controllers
 
 
 
+        //先暫時註解掉
 
         // GET: VoteRecords/Create
-        public IActionResult Create()
-        {
-            var datesQuery = from voteTime in _context.VoteTime
-                             where voteTime.ActivityID == 1
-                             select voteTime.StartDate;
+        //public IActionResult Create()
+        //{
+        //    var datesQuery = from voteTime in _context.VoteTime
+        //                     where voteTime.ActivityID == 1
+        //                     select voteTime.StartDate;
 
-            // 轉為 List
-            var dates = datesQuery.ToList();
+        //    // 轉為 List
+        //    var dates = datesQuery.ToList();
 
-            // 用於 Debug 的代碼
-            Console.WriteLine("Dates Count: " + dates.Count);
-            foreach (var date in dates)
-            {
-                Console.WriteLine("Date: " + date);
-            }
+        //    // 用於 Debug 的代碼
+        //    Console.WriteLine("Dates Count: " + dates.Count);
+        //    foreach (var date in dates)
+        //    {
+        //        Console.WriteLine("Date: " + date);
+        //    }
 
-            // 將日期傳遞到 View 中
-            ViewBag.Dates = dates;
+        //    // 將日期傳遞到 View 中
+        //    ViewBag.Dates = dates;
 
-            return View();
-        }
+        //    return View();
+        //}
 
 
 
@@ -110,23 +159,24 @@ namespace MVC_Project.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("RecordID,UserID,ActivityID,VoteResult")] VoteRecord voteRecord)
-        {
-            if (ModelState.IsValid)
-            {
-                // 設置 ActivityID 為 1
-                voteRecord.ActivityID = 1;
+        //先暫時註解掉
+        //public async Task<IActionResult> Create([Bind("RecordID,UserID,ActivityID,VoteResult")] VoteRecord voteRecord)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // 設置 ActivityID 為 1
+        //        voteRecord.ActivityID = 1;
 
-                _context.Add(voteRecord);
-                await _context.SaveChangesAsync();
+        //        _context.Add(voteRecord);
+        //        await _context.SaveChangesAsync();
 
-                return RedirectToAction(nameof(Index));
-            }
+        //        return RedirectToAction(nameof(Index));
+        //    }
 
-            ViewData["ActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID", voteRecord.ActivityID);
-            ViewData["UserID"] = new SelectList(_context.Member, "UserID", "UserID", voteRecord.UserID);
-            return View(voteRecord);
-        }
+        //    ViewData["ActivityID"] = new SelectList(_context.MyActivity, "ActivityID", "ActivityID", voteRecord.ActivityID);
+        //    ViewData["UserID"] = new SelectList(_context.Member, "UserID", "UserID", voteRecord.UserID);
+        //    return View(voteRecord);
+        //}
 
 
         // GET: VoteRecords/Edit/5
