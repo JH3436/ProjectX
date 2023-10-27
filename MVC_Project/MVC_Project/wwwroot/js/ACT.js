63ab9c0cf8dd285929ee451a67bb2d357c8ebce8
JH3436 <chemical3436@gmail.com>
2023-10-27 13:42:47 +0800

    把開關無效化

--- MVC_Project/MVC_Project/wwwroot/js/ACT.js
+++ MVC_Project/MVC_Project/wwwroot/js/ACT.js
@@ -38,124 +38,124 @@ function updateBreadcrumb(category) {
 var checkbox = document.getElementById("cktoggle_id2");
 
 // 監聽 checkbox 變化事件
-checkbox.addEventListener("change", function () {
-    // 檢查 checkbox 的狀態
-    if (checkbox.checked) {
-        // checkbox 被選中
-        console.log("Checkbox 已選中");
-        $.ajax({
-            url: '/api/loadData', // 控制器的路徑
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                console.log(data);
-                $('.cards').empty();
-                data.forEach(function (item) {
-                    var StartDate = new Date(item.StartDate).toLocaleString('en-US', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit'
+//checkbox.addEventListener("change", function () {
+//    // 檢查 checkbox 的狀態
+//    if (checkbox.checked) {
+//        // checkbox 被選中
+//        console.log("Checkbox 已選中");
+//        $.ajax({
+//            url: '/api/loadData', // 控制器的路徑
+//            type: 'GET',
+//            dataType: 'json',
+//            success: function (data) {
+//                console.log(data);
+//                $('.cards').empty();
+//                data.forEach(function (item) {
+//                    var StartDate = new Date(item.StartDate).toLocaleString('en-US', {
+//                        year: 'numeric',
+//                        month: '2-digit',
+//                        day: '2-digit'
 
-                    });
-                    var EndDate = new Date(item.EndDate).toLocaleString('en-US', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit'
+//                    });
+//                    var EndDate = new Date(item.EndDate).toLocaleString('en-US', {
+//                        year: 'numeric',
+//                        month: '2-digit',
+//                        day: '2-digit'
 
-                    });
-                    /*console.log(item.PhotoData[0].PhotoData);*/
-                    var photo = `<img class='card__img--hover' src = 'data: image/png;base64,${item.PhotoData[0].PhotoData}'></img>`
+//                    });
+//                    /*console.log(item.PhotoData[0].PhotoData);*/
+//                    var photo = `<img class='card__img--hover' src = 'data: image/png;base64,${item.PhotoData[0].PhotoData}'></img>`
 
-                    var cardHtml = `
-            <article class="card card--${item.GroupID}">
-                <div class="card__info-hover">
+//                    var cardHtml = `
+//            <article class="card card--${item.GroupID}">
+//                <div class="card__info-hover">
                 
-                    <div class="card__clock-info">
+//                    <div class="card__clock-info">
                                
-                    </div>
-                </div>
-                <div class="card__img"></div>
-                <a href="/groupPage/groupPage/${item.GroupID}" class="card_link">
-                    ${photo}
-                </a>
-                <div class="card__info">
-                    <span class="card__category">${item.GroupCategory}</span>
-                    <h3 class="card__title">${item.GroupName}</h3>
-                    <p class="card__period" style="font-size: 1.25rem;font-weight: bold;">${StartDate} ~ ${EndDate}</p>
-                    <span class="card__by">主揪<a href="#" class="card__author" title="author"> ${item.Nickname}</a></span>
-                </div>  
-            </article>
-        `;
-                    $('.cards').append(cardHtml); // 將生成的卡片添加到.cards元素中
-                });
+//                    </div>
+//                </div>
+//                <div class="card__img"></div>
+//                <a href="/groupPage/groupPage/${item.GroupID}" class="card_link">
+//                    ${photo}
+//                </a>
+//                <div class="card__info">
+//                    <span class="card__category">${item.GroupCategory}</span>
+//                    <h3 class="card__title">${item.GroupName}</h3>
+//                    <p class="card__period" style="font-size: 1.25rem;font-weight: bold;">${StartDate} ~ ${EndDate}</p>
+//                    <span class="card__by">主揪<a href="#" class="card__author" title="author"> ${item.Nickname}</a></span>
+//                </div>  
+//            </article>
+//        `;
+//                    $('.cards').append(cardHtml); // 將生成的卡片添加到.cards元素中
+//                });
                 
 
-            },
-            error: function (error) {
-                console.error(error);
-            }
-        });
+//            },
+//            error: function (error) {
+//                console.error(error);
+//            }
+//        });
 
-    } else {
-        // checkbox 未被選中
-        console.log("Checkbox 未選中");
-        $.ajax({
-            url: '/api/loadActivityData', // 控制器的路徑
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                console.log(data);
-                $('.cards').empty();
-                data.forEach(function (item) {
-                    var photo = item.OfficialPhoto[0].PhotoPath;
-                    console.log(photo);
-                    var VoteMonth = new Date(item.VoteDate).toLocaleString('en-US', {
-                        month: '2-digit'
-                    });
-                    var VoteDay = new Date(item.VoteDate).toLocaleString('en-US', {
-                        day: '2-digit'
+//    } else {
+//        // checkbox 未被選中
+//        console.log("Checkbox 未選中");
+//        $.ajax({
+//            url: '/api/loadActivityData', // 控制器的路徑
+//            type: 'GET',
+//            dataType: 'json',
+//            success: function (data) {
+//                console.log(data);
+//                $('.cards').empty();
+//                data.forEach(function (item) {
+//                    var photo = item.OfficialPhoto[0].PhotoPath;
+//                    console.log(photo);
+//                    var VoteMonth = new Date(item.VoteDate).toLocaleString('en-US', {
+//                        month: '2-digit'
+//                    });
+//                    var VoteDay = new Date(item.VoteDate).toLocaleString('en-US', {
+//                        day: '2-digit'
 
-                    });
-                    var ExpectedYear = new Date(item.ExpectedDepartureMonth).toLocaleString('en-US', {
-                        year: 'numeric',
-                    });
+//                    });
+//                    var ExpectedYear = new Date(item.ExpectedDepartureMonth).toLocaleString('en-US', {
+//                        year: 'numeric',
+//                    });
 
-                    var ExpectedMonth = new Date(item.ExpectedDepartureMonth).toLocaleString('en-US', {
-                        month: '2-digit'
-                    });
+//                    var ExpectedMonth = new Date(item.ExpectedDepartureMonth).toLocaleString('en-US', {
+//                        month: '2-digit'
+//                    });
                     
 
-                                        var cardHtml = `
-            <article class="card card--${item.ActivityID}">
-          <div class="card__info-hover">
-            <i class="fa-solid fa-envelope-open-text fa-2xl vote-icon"></i>
-            <div class="card__clock-info">
-              <div class="card__img">${item.OfficialPhotoID}</div>
-              </div>
-          </div>
+//                                        var cardHtml = `
+//            <article class="card card--${item.ActivityID}">
+//          <div class="card__info-hover">
+//            <i class="fa-solid fa-envelope-open-text fa-2xl vote-icon"></i>
+//            <div class="card__clock-info">
+//              <div class="card__img">${item.OfficialPhotoID}</div>
+//              </div>
+//          </div>
 
-          <div class="card__img" style="background-image: url(${photo})"></div>
-          <a href="/Activity/Index/${item.ActivityID}" class="card_link">
-            <div class="card__img--hover" style="background-image: url(${photo})"></div>
-          </a>
-          <span class="card__VoteDate-text">投票日：<span style="color: var(--brown)">${VoteMonth}月${VoteDay}日</span></span>
-          <div class="card__info">
-            <span class="card__category">${item.Category}</span>
-            <h3 class="card__title">${item.ActivityName}</h3>
-            <span class="card__ExpectedMonth">${ExpectedYear}-${ExpectedMonth} 月活動</span>
-          </div>
-        </article>
-`;
-                    $('.cards').append(cardHtml); // 將生成的卡片添加到.cards元素中
-                });
+//          <div class="card__img" style="background-image: url(${photo})"></div>
+//          <a href="/Activity/Index/${item.ActivityID}" class="card_link">
+//            <div class="card__img--hover" style="background-image: url(${photo})"></div>
+//          </a>
+//          <span class="card__VoteDate-text">投票日：<span style="color: var(--brown)">${VoteMonth}月${VoteDay}日</span></span>
+//          <div class="card__info">
+//            <span class="card__category">${item.Category}</span>
+//            <h3 class="card__title">${item.ActivityName}</h3>
+//            <span class="card__ExpectedMonth">${ExpectedYear}-${ExpectedMonth} 月活動</span>
+//          </div>
+//        </article>
+//`;
+//                    $('.cards').append(cardHtml); // 將生成的卡片添加到.cards元素中
+//                });
 
 
-            },
-            error: function (error) {
-                console.error(error);
-            }
-        });
+//            },
+//            error: function (error) {
+//                console.error(error);
+//            }
+//        });
 
-    }
-});
+//    }
+//});
 
